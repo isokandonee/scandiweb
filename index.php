6de34f9c2d26dcfb3bc675dc88b7201d3d93b643
Ogunniyi Isokan Mitchel <isokandone@gmail.com>
2022-05-06 22:15:10 +0100

    Starting with the front end

--- index.php
+++ index.php
@@ -5,9 +5,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Scandiweb Task</title>
+    
+    <link rel="stylesheet" href="styles/style.css">
+    <script src="scripts/script.js"></script>
 </head>
 <body>
+    <div class="header">
+        <!-- <div> -->
+            <h1>Product List</h1>
+        <!-- </div> -->
+
+        <!-- <div> -->
+            <button id="add-product-btn">Add</button>
+            <button id="deltete-product-btn">Mass Delete</button>
+        <!-- </div> -->
+    </div>
+    <hr>
+    <div class="container">
+        <div class="content"></div>
+        <div class="content"></div>
+        <div class="content"></div>
+        <div class="content"></div>
+        <div class="content"></div>
+        <div class="content"></div>
+    </div>
     
 </body>
 </html>
