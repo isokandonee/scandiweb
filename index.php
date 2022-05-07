bfba64f2e59b34f7d34adad99f8729b16bfdcb00
Ogunniyi Isokan Mitchel <isokandone@gmail.com>
2022-05-07 16:03:45 +0100

    Add product page has been added

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Scandiweb Task</title>
     
     <link rel="stylesheet" href="styles/style.css">
     <script src="scripts/script.js"></script>
+    <title>Product List Page</title>
 </head>
 <body>
     <div class="container">
@@ -20,7 +20,7 @@
 
             <!-- <div class="div2"> -->
                 <button id="add-product-btn">Add</button>
-                <button id="deltete-product-btn">Mass Delete</button>
+                <button id="delete-product-btn">Mass Delete</button>
             <!-- </div> -->
         </div>
         <hr>
---
+++ pages/add-product.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="../styles/style2.css">
+    <script src="../scripts/script.js"></script>
+    
+    <title>Product Add Page</title>
+</head>
+<body>
+    <div class="container">
+
+        <div class="header">
+            <!-- <div class="div1"> -->
+                <h1>Product List</h1>
+            <!-- </div> -->
+
+            <!-- <div class="div2"> -->
+                <button id="save-product-btn">Save</button>
+                <button id="cancel-btn">Cancel</button>
+            <!-- </div> -->
+        </div>
+        <hr>
+        <div class="box">
+
+        </div>
+
+    </div>
+    
+</body>
+</html>
