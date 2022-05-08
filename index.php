b69cb30a8af35fef770003d5110e045d187ec7f8
Ogunniyi Isokan Mitchel <isokandone@gmail.com>
2022-05-08 16:06:40 +0100

    almost done with the css

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/styles.css">
     <script src="scripts/script.js"></script>
     <title>Product List</title>
 </head>
@@ -19,7 +19,7 @@
             <!-- </div> -->
 
             <!-- <div class="div2"> -->
-                <button id="add-product-btn">Add</button>
+                <button id="add-product-btn"><a href="pages/add-product.php"> Add</a></button>
                 <button id="delete-product-btn">Mass Delete</button>
             <!-- </div> -->
         </div>
@@ -27,12 +27,93 @@
         <div class="box">
             <div class="content">
                 <input  class="delete-checkbox" type="checkbox" name="delete-checkbox" id="">
+                <ul>
+                    <li>sku</li>
+                    <li>name</li>
+                    <li>price</li>
+                    <li>size,weight or dimension</li>
+                </ul>  
             </div>
-            <div class="content"></div>
-            <div class="content"></div>
-            <div class="content"></div>
-            <div class="content"></div>
-            <div class="content"></div>
+            <div class="content">
+                <input  class="delete-checkbox" type="checkbox" name="delete-checkbox" id="">
+                <ul>
+                    <li>sku</li>
+                    <li>name</li>
+                    <li>type</li>
+                    <li>date</li>
+                    <li>done</li>
+                </ul>  
+            </div>
+            <div class="content">
+                <input  class="delete-checkbox" type="checkbox" name="delete-checkbox" id="">
+                <ul>
+                    <li>sku</li>
+                    <li>name</li>
+                    <li>type</li>
+                    <li>date</li>
+                    <li>done</li>
+                </ul>  
+            </div>
+            <div class="content">
+                <input  class="delete-checkbox" type="checkbox" name="delete-checkbox" id="">
+                <ul>
+                    <li>sku</li>
+                    <li>name</li>
+                    <li>type</li>
+                    <li>date</li>
+                    <li>done</li>
+                </ul>  
+            </div>
+            <div class="content">
+                <input  class="delete-checkbox" type="checkbox" name="delete-checkbox" id="">
+                <ul>
+                    <li>sku</li>
+                    <li>name</li>
+                    <li>type</li>
+                    <li>date</li>
+                    <li>done</li>
+                </ul>  
+            </div>
+            <div class="content">
+                <input  class="delete-checkbox" type="checkbox" name="delete-checkbox" id="">
+                <ul>
+                    <li>sku</li>
+                    <li>name</li>
+                    <li>type</li>
+                    <li>date</li>
+                    <li>done</li>
+                </ul>  
+            </div>
+            <div class="content">
+                <input  class="delete-checkbox" type="checkbox" name="delete-checkbox" id="">
+                <ul>
+                    <li>sku</li>
+                    <li>name</li>
+                    <li>type</li>
+                    <li>date</li>
+                    <li>done</li>
+                </ul>  
+            </div>
+            <div class="content">
+                <input  class="delete-checkbox" type="checkbox" name="delete-checkbox" id="">
+                <ul>
+                    <li>sku</li>
+                    <li>name</li>
+                    <li>type</li>
+                    <li>date</li>
+                    <li>done</li>
+                </ul>  
+            </div>
+            <div class="content">
+                <input  class="delete-checkbox" type="checkbox" name="delete-checkbox" id="">
+                <ul>
+                    <li>sku</li>
+                    <li>name</li>
+                    <li>type</li>
+                    <li>date</li>
+                    <li>done</li>
+                </ul>  
+            </div>
         </div>
 
     </div>
--- pages/add-product.php
+++ pages/add-product.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <link rel="stylesheet" href="../styles/style2.css">
+    <link rel="stylesheet" href="../styles/stylex.css">
     <script src="../scripts/script.js"></script>
     
     <title>Add Products</title>
@@ -25,9 +25,22 @@
         </div>
         <hr>
         <div class="box">
+            <h3>Add Products Form</h3>
             <form action="" method="post">
-                <label for="sku">SKU</label>
-                <input type="text" name="sku" id="sku">
+                <span>
+                    <label for="sku">SKU</label>
+                    <input type="text" name="sku" id="sku">
+                </span>
+                <span>
+                    <label for="sku">NAME</label>
+                    <input type="text" name="sku" id="sku">
+                </span>
+                    <label for="sku">PRICE</label>
+                    <input type="text" name="sku" id="sku">
+                    <label for="sku">SKU</label>
+                    <input type="text" name="sku" id="sku">
+                    <label for="sku">SKU</label>
+                    <input type="text" name="sku" id="sku">
             </form>
         </div>
 
